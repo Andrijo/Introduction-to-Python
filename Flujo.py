adc38e9c0e1ba2c235a22a52d091e1eeeb56c325
Andy Espino <65322580+Andrijo@users.noreply.github.com>
2024-10-28 16:31:30 -0600

    Create Flujo.py

---
+++ Flujo.py
@@ -0,0 +1,27 @@
+# --> Flujo de programa
+bandera = True
+numero = 1
+# --> Condicionales
+if bandera == False:
+    print("Bloque de código ejecutable")
+print("Fin del condicional")
+
+if 1 == 1:
+    print("Momentaneamente")
+
+if numero is int:
+    print("Momento mori")
+else:
+    print("Momentaneo")
+
+# --> Iterativos
+while(bandera == True):
+    print("Mothermommy")
+    break
+
+for i in range (5):
+    print("MWhatamoment")
+
+lista_enteros = [1, 2, 3, 4]
+for lista in lista_enteros:
+    print(lista, f"{lista}")
