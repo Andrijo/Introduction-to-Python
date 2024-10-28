a363fbef2a5694bef0519469091581bb91c9b49f
Andy Espino <65322580+Andrijo@users.noreply.github.com>
2024-10-28 16:54:23 -0600

    Create Clases.py

---
+++ Clases.py
@@ -0,0 +1,13 @@
+# --> Clases
+class Momento():
+    # Propiedad, variable dentro de una clase.
+    interna = 0
+
+    # Método
+    def imprimir(self):
+        print("Hello World!")
+        
+# Objeto 
+objeto = Momento()
+
+objeto.imprimir()
